2eb3ee9a3401ce82da2fcb7ef9f876b05cd6a9c6
niloofarmotamed <niloofar.motamed@gmail.com>
2017-11-09 11:38:59 +0200

    add state to the app

--- src/Users.js
+++ src/Users.js
@@ -6,13 +6,23 @@ class Users extends Component {
          console.log('props', this.props)
       return (
         <div className="App">
-          <ul>
-             {this.props.people.map((p)=>(
-              <li key={p.id}>
-                {p.name}
-              </li>
-             ))}
-          </ul>
+          <div className="title">
+            <h3> Name </h3>
+            <h3> Email </h3>
+            <h3> Phone Number</h3>
+          </div>
+            <ul className="people-list">
+              {this.props.people.map((p)=>(
+                <li key={p.id} className="list-item">
+                 <div className="details">
+                    <p>{p.name}</p>
+                    <p>{p.email}</p>
+                    <p>{p.phone}</p>
+                    <button className="remove"></button>
+                 </div>
+                </li>
+              ))}
+            </ul>          
         </div>
       );
     }
