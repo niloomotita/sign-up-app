d3658c6ab2a46520764ef551d92e88e3a7c39d01
niloofarmotamed <niloofar.motamed@gmail.com>
2017-11-10 11:41:58 +0200

    add handleSubmit to serialze the form and pass the input values to addParticipant method

--- src/AddUser.js
+++ src/AddUser.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import serializeForm from 'form-serialize';
 
 class AddUser extends Component {
+    handleSubmit =(e)=>{
+        e.preventDefault()
+        const values = serializeForm(e.target, {hash: true})
+        this.props.onAddUser(values);
+    }
     render(){
         return(
         <div className="add-user">
-            <form action="">
+            <form onSubmit={this.handleSubmit}>
                 <input type="text" name="name"  placeholder=" name "/>
                 <input type="text" name="email"  placeholder=" E-mail address "/>
                 <input type="text" name="phone"  placeholder=" phone number "/>
